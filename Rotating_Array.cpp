125a3baf780efd90403d0f7531ae35274c7a6556
Siddharth Tripathi <prahladtripathi123@gmail.com>
2019-09-03 00:39:20 +0530

    Add files via upload

---
+++ Rotating_Array.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,d;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            
+        }
+        cin>>d;
+        
+        for(int i=d;i<n;i++)
+        {
+            cout<<a[i]<<" ";
+        }
+        for(int i=0;i<d;i++)
+        {
+            cout<<a[i]<<" ";
+        }
+        
+       cout<<"\n";
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+return 0;    
+}
+
